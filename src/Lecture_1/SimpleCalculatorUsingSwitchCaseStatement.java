409dc95c9195e02c0ffcc83720917e9f204db8d5
Shubham <shubhamsd53@gmail.com>
2022-07-29 17:51:00 +0530

    Calculator using switch case statement

---
+++ src/Lecture_1/SimpleCalculatorUsingSwitchCaseStatement.java
@@ -0,0 +1,32 @@
+package Lecture_1;
+
+import java.util.Scanner;
+
+public class SimpleCalculatorUsingSwitchCaseStatement {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n1 = scn.nextInt();
+        String op = scn.next();
+        int n2 = scn.nextInt();
+
+        switch (op) {
+            case "+":
+                System.out.println(n1 + n2);
+                break;
+            case "-":
+                System.out.println(n1 - n2);
+                break;
+            case "/":
+                System.out.println(n1 / n2);
+                break;
+            case "*":
+                System.out.println(n1 * n2);
+                break;
+            case "%":
+                System.out.println(n1 % n2);
+                break;
+            default:
+                System.out.println("Invalid Input");
+        }
+    }
+}
