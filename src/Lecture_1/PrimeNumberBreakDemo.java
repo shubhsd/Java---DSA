4ae9f9786aa354cdae32c9b6f36adc97b3daf5fb
Shubham <shubhamsd53@gmail.com>
2022-07-29 17:11:01 +0530

    Check Prime number for multiple inputs

---
+++ src/Lecture_1/PrimeNumberBreakDemo.java
@@ -0,0 +1,24 @@
+package Lecture_1;
+
+import java.util.Scanner;
+
+public class PrimeNumberBreakDemo {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+
+        //        Check if n is prime or not
+        int i = 2;
+        for (; i <= number - 1; i++) {
+            if (number % i == 0) {
+                break;
+            }
+        }
+//        check value of i
+        if (i == number) {
+            System.out.println("Prime");
+        } else {
+            System.out.println("Not prime");
+        }
+    }
+}
---
+++ src/Lecture_1/PrimeNumberForMultipleInputs.java
@@ -0,0 +1,31 @@
+package Lecture_1;
+
+import java.util.Scanner;
+
+public class PrimeNumberForMultipleInputs {
+    public static void main(String[] args) {
+//        T inputs
+//        For each input print prime or not
+//        T = 5
+//        Input 7 11 4 8 13
+//        Prime, Prime, not prime, not prime, prime
+
+        Scanner scn = new Scanner(System.in);
+        int T = scn.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int n = scn.nextInt();
+            int i = 2;
+            for (; i <= n - 1; i++) {
+                if (n % i == 0) {
+                    break;
+                }
+            }
+//        check value of i
+            if (i == n) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not prime");
+            }
+        }
+    }
+}
