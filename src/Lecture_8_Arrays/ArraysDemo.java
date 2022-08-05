efc77ae21954b82fb50becdbef47c1063f81cbbe
Shubham <shubhamsd53@gmail.com>
2022-08-05 21:09:33 +0530

    Creating new array

--- src/Lecture_8_Arrays/ArraysDemo.java
+++ src/Lecture_8_Arrays/ArraysDemo.java
@@ -1,11 +1,12 @@
 package Lecture_8_Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraysDemo {
-    public static void main(String[] args) {
-//        1. Hardcoded Array
-        int[] marks = {10, 20, 30, 40, 50};
+
+    public static void playWithArray() {
+        int[] marks = {10, 50, 30, 40, 50};
         System.out.println(marks);
         System.out.println(marks.length);
 //        Printing array
@@ -24,6 +25,23 @@ public class ArraysDemo {
 //        Method 3 to print array using Arrays.toString()
         System.out.println("Using Arrays.toString()");
         System.out.println(Arrays.toString(marks));
+    }
+    public static void main(String[] args) {
+//        1. Hardcoded Array
+//        System.out.println("Hardcoded Array " );
+//        playWithArray();
+
 //        2. User Input array
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+//        Creating array of length n
+        int[] newArr = new int[n];
+
+        int[] arr = new int[5];
+        System.out.println(arr);
+        System.out.println(arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 }
