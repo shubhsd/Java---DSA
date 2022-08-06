268e9f521e37ddc471ec0916aa7672806e97739e
Shubham <shubhamsd53@gmail.com>
2022-08-06 09:05:15 +0530

    Largest number in an array

---
+++ src/Lecture_8_Arrays/FindLargestNumberInArray.java
@@ -0,0 +1,37 @@
+package Lecture_8_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FindLargestNumberInArray {
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int largest(int[] arr) {
+        int largestNum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (largestNum < arr[i]) {
+                largestNum = arr[i];
+            }
+        }
+        return largestNum;
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        printArray(arr);
+        int largestNum = largest(arr);
+        System.out.println("Largest number present in array is " + largestNum);
+    }
+}
