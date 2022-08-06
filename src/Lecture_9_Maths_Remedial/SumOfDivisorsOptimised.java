c30415005729985e7410714c9a0e9eeb35c13f06
Shubham <shubhamsd53@gmail.com>
2022-08-06 13:26:32 +0530

    Optimised - sum of divisors

---
+++ src/Lecture_9_Maths_Remedial/SumOfDivisorsOptimised.java
@@ -0,0 +1,17 @@
+package Lecture_9_Maths_Remedial;
+
+import java.util.Scanner;
+
+public class SumOfDivisorsOptimised {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sum = 0;
+        for (int i = 1; i*i <= n; i++) {
+            if (n % i == 0) {
+                sum = sum + i + (n / i);
+            }
+        }
+        System.out.println(sum);
+    }
+}
