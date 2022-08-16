f67137b3b5817c896a5df324dc0588ca2545a72b
Shubham <shubhamsd53@gmail.com>
2022-08-16 12:07:45 +0530

    Basic arraylist demo
    Co-Authored-By: Shubham <12894520+shubhsd@users.noreply.github.com>

---
+++ src/Lecture_10_2D_Arrays/ArrayListsDemo.java
@@ -0,0 +1,18 @@
+package Lecture_10_2D_Arrays;
+
+import java.util.ArrayList;
+
+public class ArrayListsDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(9);
+        arr.add(0, 4);
+
+//        Print all elements
+
+        for(int i = 0; i<arr.size(); i++) {
+            System.out.println(arr.get(i));
+        }
+    } 
+}
