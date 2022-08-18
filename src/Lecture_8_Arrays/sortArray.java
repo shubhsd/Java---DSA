f3ec13f56cde994599d2b13a8e697588d0a930a2
Shubham <shubhamsd53@gmail.com>
2022-08-18 20:56:38 +0530

    Sort array using inbuilt function

---
+++ src/Lecture_8_Arrays/sortArray.java
@@ -0,0 +1,11 @@
+package Lecture_8_Arrays;
+
+import java.util.Arrays;
+
+public class sortArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 10, 9, 8, 6, 4, 3, 2};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
